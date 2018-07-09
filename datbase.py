5332af4e88387eafa81e1ca1cef6eb811f16e8c5
SwapCyb <40315265+SwapCyb@users.noreply.github.com>
2018-07-09 16:35:48 +0530

    Delete datbase.py

--- datbase.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-import pandas as pd
-Client = MongoClient()
-coaching_info = Client.mydatabase
-grotal_coaching = coaching_info.grotal_coaching
-df1 = pd.DataFrame(list(grotal_coaching.find()))
-df1.to_csv('grotal_csvfile.txt', sep='\t', encoding='utf-8')
